396878a6b24559bda4798a397ff0f9630f70d9c2
Jamie Har <jlh493@cornell.edu>
2019-04-25 17:03:28 -0400

    testimonial role

--- includes/testimonial_form.php
+++ includes/testimonial_form.php
@@ -8,10 +8,17 @@
         <label for="rating">Rating:</label>
         <select name="rating" id="rating">
             <?php
-            for ($i=1; $i<=5; $i++) {
-                echo "<option value='".$i."'>".$i."</option>";
+            for ($i = 1; $i <= 5; $i++) {
+                echo "<option value='" . $i . "'>" . $i . "</option>";
             }
             ?>
+        </select>
+    </div>
+    <div>
+        <label for="role">Role:</label>
+        <select name="role" id="role">
+            <option value="parent">Parent</option>
+            <option value="student">Student</option>
         </select>
     </div>
     <button type="submit" name="submit_testimony">Submit</button>
