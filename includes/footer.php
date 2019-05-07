d6024fadef7ca69d6fd31f83187173cdd2baad1f
Jamie Har <jlh493@cornell.edu>
2019-05-07 06:24:27 -0400

    location office to footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,10 +1,14 @@
 <div class="footer">
-    <div class='phone'>
+    <div>
         <a href="tel:305-926-5537"><img class="icon" src="images/phone.png" alt="phone" /></a>
         <p>(305)-926-5537</p>
     </div>
-    <div class='email'>
+    <div>
         <a href="mailto:lmitrani114@gmail.com"><img class="icon" src="images/email.png" alt="email" /></a>
         <p>lmitrani114@gmail.com</p>
+    </div>
+    <div>
+        <a href="https://goo.gl/maps/EhxHHmLNNtDRDL6F7" target="_blank"><img class="icon" src="images/location.png" alt="office" /></a>
+        <p>301 Arthur Godfrey Rd. Penthouse, Miami Beach, FL 33140</p>
     </div>
 </div>
