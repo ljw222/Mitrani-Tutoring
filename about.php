cf4b2b523724a78cb549f591a86ec2ea05c7cf8a
Jamie Har <jlh493@cornell.edu>
2019-05-07 06:08:08 -0400

    contacts on about, footer

--- about.php
+++ about.php
@@ -34,12 +34,23 @@ include("includes/init.php");
       <div class="about_me">
         <!-- <p class="quote">I make learning fun, with proven results!</p> -->
         <p>My name is Laurie Mitrani, and I am a <strong>compassionate, child-oriented tutor</strong> who makes learning fun. I have been a <strong>certified teacher</strong> for 30+ years, and have a wide variety of teaching experience. I graduated with a Masters in Education from the University of Virginia, and have experience teaching preschool through high school age students. I have been a elementary classroom teacher, assistant principal, and a principal. In addition, I have <strong>specialized training</strong> in reading. I have been trained in the Orton-Gillingham Approach to help students with reading impairments such as dyslexia. I have also been trained in the Lindamood Bell Approach to help students with reading and comprehension.</p>
-        <h2>Contacts</h2>
-        <ul>
-          <li>Email: lmitrani114@gmail.com</li>
-          <li>Phone: (305)-926-5537</li>
-          <li>LinkedIn: <a href="https://www.linkedin.com/in/laurie-mitrani-a69b28157">Laurie Mitrani - Educational Consultant and Tutor at Mitrani Tutoring</a></li>
-        </ul>
+        <div id="whole-about-contacts">
+          <h2>Contacts</h2>
+          <div id="about-contacts">
+            <div>
+              <a href="mailto:lmitrani114@gmail.com"><img class="contact-icon" src="images/email-black.png" alt="email" /></a>
+              <p>lmitrani114@gmail.com</p>
+            </div>
+            <div>
+              <a href="tel:305-926-5537"><img class="contact-icon" src="images/phone-black.png" alt="phone" /></a>
+              <p>(305)-926-5537<p>
+            </div>
+            <div>
+              <a href="https://www.linkedin.com/in/laurie-mitrani-a69b28157" target='_blank'><img class="contact-icon" src="images/linkedin.png" alt="linkedin" /></a>
+              <p>Laurie Mitrani on LinkedIn</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
--- includes/footer.php
+++ includes/footer.php
@@ -1,10 +1,10 @@
 <div class="footer">
-    <div class='phone'>"
-        <img class="icon" src="images/phone.png" alt="phone" />
+    <div class='phone'>
+        <a href="tel:305-926-5537"><img class="icon" src="images/phone.png" alt="phone" /></a>
         <p>(305)-926-5537</p>
     </div>
     <div class='email'>
-        <img class="icon" src="images/email.png" alt="email" />
+        <a href="mailto:lmitrani114@gmail.com"><img class="icon" src="images/email.png" alt="email" /></a>
         <p>lmitrani114@gmail.com</p>
     </div>
 </div>
